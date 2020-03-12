5b252a5b0057aad13592169fc1cc05151eca55a4
PrithvirajNeueda <61828988+PrithvirajNeueda@users.noreply.github.com>
2020-03-12 11:27:48 +0000

    update init py

--- ServiceBusQueueTrigger1/__init__.py
+++ ServiceBusQueueTrigger1/__init__.py
@@ -15,7 +15,7 @@ def sd_xmltojson_function(msgIn: func.ServiceBusMessage, msgOut: func.Out[str]):
         strxml= msgIn.get_body().decode('utf-8')
         logging.info('Received xml from sdxmlqueue: %s', strxml)
         outMsgDict = xmltodict.parse(strxml, namespaces=namespaces, cdata_key='_value')
-        logging.debug('Converted xml to json message: %s', outMsgDict)
+        logging.debug('Converted xml to json dictionary message: %s', outMsgDict)
         msgOut.set(json.dumps(outMsgDict))
     except Exception as e:
         logging.error('Failed to parse xml: {}'.format(str(e)))
